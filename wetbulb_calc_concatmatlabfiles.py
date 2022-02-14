0cbd89d6270079924dc956f4c3ef5adee74f46a1
Sarah.Chapman <earsch@leeds.ac.uk>
2022-02-14 08:51:49 +0000

    Changed so worked with CP4A, changed var name from p25_temp to temp_orig.

--- wetbulb_calc_concatmatlabfiles.py
+++ wetbulb_calc_concatmatlabfiles.py
@@ -40,11 +40,14 @@ import WetBulb as wb
 proj = ccrs.PlateCarree(central_longitude = 38)
 
 #%% Import data
+#update model name and scenario name in filenames, and saved path at end of script
 
-p25_temp = iris.load_cube('/nfs/a321/earsch/Tanga/Data/CP4_Processed/tas/tas_day_p25_historical.nc')
+#temp_orig = iris.load_cube('/nfs/a321/earsch/Tanga/Data/CP4_Processed/tas/tas_day_p25_rcp85.nc')
+temp_orig = iris.load_cube('/nfs/a321/earsch/Tanga/Data/CP4_Processed/tas/tas_day_cp4_historical_p25grid.nc')
+
 
 file_path = ('/nfs/a321/earsch/floods_heatwaves/processed/wetbulb_temp/')
-filenames = glob.glob(file_path + '*p25*part?.nc')
+filenames = glob.glob(file_path + '*cp4_hist*part?.nc')
 files = iris.cube.CubeList()
 for file in filenames:
     x = iris.load_cube(file)
@@ -69,27 +72,31 @@ max_lon = 16.0
 cons = iris.Constraint(latitude = lambda cell: min_lat < cell < max_lat,
                        longitude = lambda cell: min_lon < cell < max_lon)
 
-p25_temp = p25_temp.extract(cons)
+temp_orig = temp_orig.extract(cons)
 
-p25_temp.coord('longitude').guess_bounds()
-p25_temp.coord('latitude').guess_bounds()
+try:
+    temp_orig.coord('longitude').guess_bounds()
+    temp_orig.coord('latitude').guess_bounds()
+except:
+    print('has bounds')
+    
 concat_files.coord('longitude').guess_bounds()
 concat_files.coord('latitude').guess_bounds()
 
-cs = p25_temp.coord_system(iris.coord_systems.CoordSystem)
+cs = temp_orig.coord_system(iris.coord_systems.CoordSystem)
 concat_files.coord('longitude').coord_system = cs
 concat_files.coord('latitude').coord_system = cs
 
-concat_regrid = concat_files.regrid(p25_temp, iris.analysis.AreaWeighted())
+concat_regrid = concat_files.regrid(temp_orig[0], iris.analysis.AreaWeighted())
 
 
 #%%
 #put data fromc conat files into P25 temp, so get coord info
 
-p25_temp.data[0:3000, :,:] = concat_regrid.data
+temp_orig.data[0:3600, :,:] = concat_regrid.data
 
 
 #%%
 
-iris.save(p25_temp, '/nfs/a321/earsch/floods_heatwaves/processed/wetbulb_temp/wb_p25_daily_historical_wa.nc')
+iris.save(temp_orig, '/nfs/a321/earsch/floods_heatwaves/processed/wetbulb_temp/wb_cp4_daily_historical_wa.nc')
 
