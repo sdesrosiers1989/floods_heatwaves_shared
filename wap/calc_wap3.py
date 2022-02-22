78b985ddd6e03406d199f41480d242ad59d2f94d
Sarah.Chapman <earsch@leeds.ac.uk>
2022-02-22 10:17:18 +0000

    Removed loop for doing pan-africa in sections - not calculating correctly

--- wap/calc_wap3.py
+++ wap/calc_wap3.py
@@ -210,35 +210,14 @@ print('Window', window, sep = ' ')
 print('Alpha', alpha, sep = '')
 
 
-if area == 'wa':
-    wap_cube = apply_wap(pr, window, alpha, ls_regrid)
 
-    #save data
-    
-    print('Saving data')
-    save_name = save_path + mod + '_' + scen + '_w' + str(window) + '_a' + str(alpha) + '.nc'
-    
-    iris.save(wap_cube, save_name)
-else:
-    #for pan africa split into parts (need to do all timesteps at once for wap due to window)
-    dims = pr.shape
-    
-    start_idx = np.arange(0, dims[1], 50)
-    end_idx = start_idx + 50
-    end_idx[-1] = dims[1]
-    
-    n_parts = len(start_idx)
-    for k in np.arange(0, n_parts):
-        print('Starting wap part ', k, start_idx[k], end_idx[k])
-        new_pr = pr[:, start_idx[k]:end_idx[k], :]
-        wap_cube = apply_wap(new_pr, window, alpha, ls_regrid)
-        
-        print('Saving wap part ', k)
-        save_name = save_path + 'partial_files/' + mod + '_' + scen + '_w' + str(window) + '_a' + str(alpha) + '_part' + str(k) + '.nc'
-        
-        iris.save(wap_cube, save_name)
-    
+wap_cube = apply_wap(pr, window, alpha, ls_regrid)
 
-    
+#save data
+
+print('Saving data')
+save_name = save_path + mod + '_' + scen + '_w' + str(window) + '_a' + str(alpha) + '.nc'
+
+iris.save(wap_cube, save_name)
 
 
