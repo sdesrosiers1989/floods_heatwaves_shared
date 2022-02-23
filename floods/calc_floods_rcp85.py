c8bd0e238ec106d2dd06b208ed4da90f7781a410
Sarah.Chapman <earsch@leeds.ac.uk>
2022-02-23 08:22:04 +0000

    Removed flood functions and referred to separate file to import flood functions

--- floods/calc_floods_rcp85.py
+++ floods/calc_floods_rcp85.py
@@ -41,11 +41,8 @@ import matplotlib.pyplot as plt
 
 #Import my functions
 import sys
-sys.path.append('/nfs/see-fs-02_users/earsch/Documents/Leeds/Repos/Tanga/Plot_functions')
-import tanzania1 as tp
-sys.path.append('/nfs/see-fs-02_users/earsch/Documents/Leeds/Repos/Tanga/Onset_functions')
-from onset_functions import masking
-from onset_functions import find_season
+sys.path.append('/nfs/see-fs-02_users/earsch/Documents/Leeds/Repos/floods_heatwaves/floods_heatwaves_shared/floods/')
+import flood_functions as f
 
 proj = ccrs.PlateCarree(central_longitude = 38)
 
@@ -111,119 +108,7 @@ ls_regrid = ls_regrid.extract(cons)
 #will give 9 years exactly, starting 1st juen 1997, ending 1st june (exclusive)
 wap = wap[150:3390,:,:]
 
-
-#%% calc threshold
-
-
-def calc_thres(cube, per, save_path, mod, scen, save = True):
-    thres = cube.collapsed('time', iris.analysis.PERCENTILE, percent = per)
-    
-    if save == True:
-        save_name = save_path + mod + '_' + scen + '_' + str(per) + '.nc'
-        iris.save(thres, save_name)
         
-    return thres
-
-
-def get_floods(cube, wap_thres, ls):
-    ''' Calculate floods events
-        Defined as wap > 95th percentile
-        Calcualted only for land points
-        
-        Find duration, number of flood events, flood days,
-        and start and end point of floods
-    
-        cube = input weighted area percip cube
-        wap_thres = wap threshold which defines floods (i.e., 95th percnentile)
-        ls = landsea mask
-
-    '''
-    
-    ## Create cubes to save output data
-    
-    shape=np.shape(cube)
-    nt=shape[0]
-    nlat=shape[1]
-    nlon=shape[2]
-
-    # total number of flood days and individual flodo events for each gridpoint within time series
-    count_flood_days = copy.deepcopy(cube[0])
-    count_flood_days.data = np.zeros((nlat,nlon))
-    count_flood_event = copy.deepcopy(cube[0])
-    count_flood_event.data = np.zeros((nlat,nlon))
-    # will create cube with for duration of flood
-    # at first day of each flood (0s everything else)
-    flood_duration = copy.deepcopy(cube)
-    flood_duration.data = np.zeros((nt,nlat,nlon))
-    #start and and end day of floods
-    start_flood_event = copy.deepcopy(cube)
-    start_flood_event.data = np.zeros((nt,nlat,nlon))
-    end_flood_event = copy.deepcopy(cube)
-    end_flood_event.data = np.zeros((nt,nlat,nlon))
- 
-    
-    #find flood events
-    floods = copy.deepcopy(cube)
-    floods.data = np.where(floods.data > wap_thres.data, 1.0, 0.0)     
-
-    for y in range(nlat):
-        for x in range(nlon):
-            if ls[y,x].data > 0.5:
-                
-                f_loc = floods[:, y, x].data
-                 
-                f=np.zeros(nt+2) # add an element at beginning and end compared to floods
-                
-                #find locations of floods
-                fidx = np.where(f_loc == 1.0)
-                
-                f[fidx[0] + 1] = 1 # add 1 to every index after flood defined
-
-                diffs=np.diff(f) 
-                # this is 1 where it becomes flood and -1 where it becomes not flood 
-                # and 0 where it stays the same
-                
-                # get indices where changes occur and find length of flood events
-                start_flood=np.where(diffs>0)[0]
-                end_flood=np.where(diffs<0)[0] # end flood day - day flood ends (not a flood day)
-                nstart=len(start_flood)
-                #nend=len(end_hot)
-                len_flood=end_flood-start_flood
-                
-               
-                #get total num flood days
-                n_flood_days = np.sum(len_flood)
-                count_flood_days.data[y,x] = n_flood_days
-                
-                #number of individual flood events
-                count_flood_event.data[y,x] = nstart
-
-
-
-                # flood_duration:  cube with length of flood at first day of flood
-                #  start flood
-                #  end flood
-                for i in range(nstart):
-    
-                    #flood duration
-                    flood_duration.data[start_flood[i], y,x]=len_flood[i]
-                    
-                    
-                    #assign 1s to start and end dates of heatwaves
-                    start_flood_event.data[start_flood[i], y,x] = 1
-                    end_flood_event.data[end_flood[i] - 1, y,x] = 1 # last day of flood, 
-                                                                    # is a flood day
-                    
-          
-                        
-                        
-                        
-
-    return [floods, flood_duration, count_flood_days, count_flood_event, start_flood_event, end_flood_event]
-
-
-
-                
 
 #%%
 
@@ -234,14 +119,14 @@ def get_floods(cube, wap_thres, ls):
 if scen == 'rcp85':
     per_95 = iris.load_cube('/nfs/a321/earsch/floods_heatwaves/processed/wap/%s/thres/%s_historical_95.nc' % (loc, mod))
 else:
-    per_95 = calc_thres(wap, 95, save_path_thres, mod, scen)
+    per_95 = f.calc_thres(wap, 95, save_path_thres, mod, scen)
 
 #%%
 
 
 
 #floods
-output = get_floods(wap, per_95, ls_regrid)
+output = f.get_floods(wap, per_95, ls_regrid)
 
 
 #save data
@@ -250,6 +135,6 @@ var_names = ['floods', 'duration', 'ndays', 'nevents', 'start', 'end']
 
 for i in np.arange(len(output)):
 
-    save_name = save_path +  var_names[i] + '_' + mod + '_' + scen + '.nc'
+    save_name = save_path +  var_names[i] + '_' + mod + '_' + scen + '_r' + str(restrict) +'.nc'
 
     iris.save(output[i], save_name)
