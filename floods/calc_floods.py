b20588b2da2de2e8aff28196f356dc9d17fcc798
Sarah.Chapman <earsch@leeds.ac.uk>
2022-02-23 08:20:01 +0000

    Removed functions into own file

--- floods/calc_floods.py
+++ floods/calc_floods.py
@@ -21,8 +21,6 @@ import iris
 import iris.plot as iplt
 import iris.quickplot as qplt
 import iris.coord_categorisation
-from iris.util import rolling_window
-from iris.analysis import Aggregator
 
 from cf_units import Unit
 
@@ -35,17 +33,12 @@ import copy
 
 import glob
 
-import pandas as pd
-
-import matplotlib.pyplot as plt
 
 #Import my functions
 import sys
-sys.path.append('/nfs/see-fs-02_users/earsch/Documents/Leeds/Repos/Tanga/Plot_functions')
-import tanzania1 as tp
-sys.path.append('/nfs/see-fs-02_users/earsch/Documents/Leeds/Repos/Tanga/Onset_functions')
-from onset_functions import masking
-from onset_functions import find_season
+sys.path.append('/nfs/see-fs-02_users/earsch/Documents/Leeds/Repos/floods_heatwaves/floods_heatwaves_shared/floods/')
+import flood_functions as f
+
 
 proj = ccrs.PlateCarree(central_longitude = 38)
 
@@ -58,7 +51,7 @@ save_path = '/nfs/a321/earsch/floods_heatwaves/processed/wap/pan_africa/floods/'
 save_path_thres = '/nfs/a321/earsch/floods_heatwaves/processed/wap/pan_africa/thres/'  
 mod = 'p25'
 scen = 'historical'
-area = 'pan_africa'
+area = 'west_africa'
 
 fname = '/nfs/a321/earsch/floods_heatwaves/processed/wap/' + area + '/wap/' + mod + '_' + scen + '*.nc'
 wap = iris.load_cube(fname)
@@ -111,137 +104,18 @@ if area == 'west_africa':
 wap = wap[150:3390,:,:]
 
 
-#%% calc threshold
 
-
-def calc_thres(cube, per, save_path, mod, scen, save = True):
-    thres = cube.collapsed('time', iris.analysis.PERCENTILE, percent = per)
-    
-    if save == True:
-        save_name = save_path + mod + '_' + scen + '_' + str(per) + '.nc'
-        iris.save(thres, save_name)
-        
-    return thres
-
-
-def get_floods(cube, wap_thres, ls, restrict = 0.0):
-    ''' Calculate floods events
-        Defined as wap > 95th percentile
-        Calcualted only for land points
-        
-        Find duration, number of flood events, flood days,
-        and start and end point of floods
-    
-        cube = input weighted area percip cube
-        wap_thres = wap threshold which defines floods (i.e., 95th percnentile)
-        ls = landsea mask
-
-    '''
-    
-    ## Create cubes to save output data
-    
-    shape=np.shape(cube)
-    nt=shape[0]
-    nlat=shape[1]
-    nlon=shape[2]
-
-    # total number of flood days and individual flodo events for each gridpoint within time series
-    count_flood_days = copy.deepcopy(cube[0])
-    count_flood_days.data = np.zeros((nlat,nlon))
-    count_flood_event = copy.deepcopy(cube[0])
-    count_flood_event.data = np.zeros((nlat,nlon))
-    # will create cube with for duration of flood
-    # at first day of each flood (0s everything else)
-    flood_duration = copy.deepcopy(cube)
-    flood_duration.data = np.zeros((nt,nlat,nlon))
-    #start and and end day of floods
-    start_flood_event = copy.deepcopy(cube)
-    start_flood_event.data = np.zeros((nt,nlat,nlon))
-    end_flood_event = copy.deepcopy(cube)
-    end_flood_event.data = np.zeros((nt,nlat,nlon))
- 
-    
-    #find flood events
-    floods = copy.deepcopy(cube)
-    floods.data = np.where(floods.data > wap_thres.data, 1.0, 0.0)     
-    
-    wap_greater_restrict = copy.deepcopy(cube)
-    wap_greater_restrict.data = np.where(wap_greater_restrict.data >= restrict, 1.0, 0.0) 
-
-    for y in range(nlat):
-        for x in range(nlon):
-            if ls[y,x].data > 0.5:
-        
-                
-                f_loc = floods[:, y, x].data
-                r_loc = wap_greater_restrict[:,y,x].data
-                 
-                f=np.zeros(nt+2) # add an element at beginning and end compared to floods
-                
-                #find locations of floods (both threshold and mm restirction are true)
-                fidx = np.where(f_loc + r_loc == 2.0)
-           
-                f[fidx[0] + 1] = 1 # add 1 to every index after flood defined
-
-                diffs=np.diff(f) 
-                # this is 1 where it becomes flood and -1 where it becomes not flood 
-                # and 0 where it stays the same
-                
-                # get indices where changes occur and find length of flood events
-                start_flood=np.where(diffs>0)[0]
-                end_flood=np.where(diffs<0)[0] # end flood day - day flood ends (not a flood day)
-                nstart=len(start_flood)
-                #nend=len(end_hot)
-                len_flood=end_flood-start_flood
-                
-               
-                #get total num flood days
-                n_flood_days = np.sum(len_flood)
-                count_flood_days.data[y,x] = n_flood_days
-                
-                #number of individual flood events
-                count_flood_event.data[y,x] = nstart
-
-
-
-                # flood_duration:  cube with length of flood at first day of flood
-                #  start flood
-                #  end flood
-                for i in range(nstart):
-    
-                    #flood duration
-                    flood_duration.data[start_flood[i], y,x]=len_flood[i]
-                    
-                    
-                    #assign 1s to start and end dates of heatwaves
-                    start_flood_event.data[start_flood[i], y,x] = 1
-                    end_flood_event.data[end_flood[i] - 1, y,x] = 1 # last day of flood, 
-                                                                    # is a flood day
-                    
-          
-                        
-                        
-                        
-
-    return [floods, flood_duration, count_flood_days, count_flood_event, start_flood_event, end_flood_event]
-
-
-
-                
-
-#%%
-
-#calculate flods
+#%% #calculate threshold
 
 #thres    
-per_95 = calc_thres(wap, 95, save_path_thres, mod, scen)
+per_95 = f.calc_thres(wap, 95, save_path_thres, mod, scen)
 
-#%%
+#%% calcualte floods
 
 restrict = 10.0
 
 #floods
-output = get_floods(wap, per_95, ls_regrid)
+output = f.get_floods(wap, per_95, ls_regrid)
 
 
 #save data
